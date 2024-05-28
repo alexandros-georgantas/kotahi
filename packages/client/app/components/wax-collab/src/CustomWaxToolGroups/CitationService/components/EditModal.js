b375ba6330c6326c03fbd2ec533dd9807d3883ee
Vignesh Devendran <vigneshdevendran7@gmail.com>
2024-05-28 15:12:17 +0000

    Resolve "Citation parser should preserve reference list ordering"

--- packages/client/app/components/wax-collab/src/CustomWaxToolGroups/CitationService/components/EditModal.js
+++ packages/client/app/components/wax-collab/src/CustomWaxToolGroups/CitationService/components/EditModal.js
@@ -21,8 +21,7 @@ const EditModal = ({
 }) => {
   // console.log('Coming in: ', citationData, formattedCitation)
   const config = useContext(ConfigContext)
-  /* eslint-disable-next-line no-unsafe-optional-chaining */
-  const { styleName } = config?.production
+  const styleName = config?.production?.styleName || 'apa'
   const { t } = useTranslation()
   const [currentText, setCurrentText] = useState(formattedCitation)
   const [reRender, setReRender] = useState(-1)
